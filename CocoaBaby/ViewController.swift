23ccd25827fbd4264c2e849c1e02838a7b4af79e
dgSohn <nhos126@gmail.com>
2017-08-02 13:57:49 +0900

    first commit

---
+++ CocoaBaby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaBaby
+//
+//  Created by Sohn on 02/08/2017.
+//  Copyright © 2017 Sohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
