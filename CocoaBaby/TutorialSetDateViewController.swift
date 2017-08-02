42f0b0039f4b23e8628c35062a730d895616c4e9
LeFal <qwertyhj2@gmail.com>
2017-08-02 16:49:10 +0900

    date picker update

--- CocoaBaby/TutorialSetDateViewController.swift
+++ CocoaBaby/TutorialSetDateViewController.swift
@@ -8,32 +8,68 @@
 
 import UIKit
 
-class TutorialSetDateViewController: UIViewController {
+class TutorialSetDateViewController: UIViewController, UITextFieldDelegate {
 
     var babyName : String?
     var pregnantDate : Date?
     var birthDate : Date?
     
+    @IBOutlet weak var pregnantDateTextField: UITextField!
+    @IBOutlet weak var birthDateTextField: UITextField!
+    var datePicker : UIDatePicker!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pregnantDateTextField.delegate = self
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func pickUpDate(_ textField : UITextField){
+        
+        // DatePicker
+        self.datePicker = UIDatePicker(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker.backgroundColor = UIColor.white
+        self.datePicker.datePickerMode = UIDatePickerMode.date
+        textField.inputView = self.datePicker
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(self.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
     }
-    */
+
+    func doneClick() {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        pregnantDateTextField.text = dateFormatter1.string(from: datePicker.date)
+        pregnantDateTextField.resignFirstResponder()
+    }
+    func cancelClick() {
+        pregnantDateTextField.resignFirstResponder()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickUpDate(self.pregnantDateTextField)
+    }
+
+
+    
 
 }
