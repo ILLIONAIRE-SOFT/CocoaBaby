7e24d7d6dfffc6740494b542812073026c41e0dc
dadong <iluvdadong@naver.com>
2017-08-02 17:33:41 +0900

    merge

--- CocoaBaby/ViewController/HomeViewController.swift
+++ CocoaBaby/ViewController/HomeViewController.swift
@@ -9,52 +9,57 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     @IBOutlet var babyView: UIView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var dDayLabel: UILabel!
-
+    @IBOutlet var infoLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-       // updateBabyView()
-        //updateBabyInfo()
-
+        BabyStore.shared.loadBaby()
+        
+       
+        updateBabyInfo()
+        
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        super.viewDidAppear(animated)
         
-        BabyStore.shared.loadBaby()
+         updateBabyView()
         
         if BabyStore.shared.baby == nil {
             let tutorialStoryboard = UIStoryboard(name: "Tutorial", bundle: nil)
             let viewController = tutorialStoryboard.instantiateViewController(withIdentifier: "tutorialNavigationViewController")
             self.present(viewController, animated: false, completion: nil)
         }
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     private func updateBabyInfo() {
-    
+        
         self.nameLabel.text = BabyStore.shared.getName()
         self.dDayLabel.text = "D-\(BabyStore.shared.getDday())"
-    
+        self.infoLabel.text = "1주차에는 5대 영양소를 골고루!"
     }
     
     private func updateBabyView() {
         
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "HomeGradation")!)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Gradation2")!)
         babyView.layer.cornerRadius = babyView.frame.width/2
     }
     
