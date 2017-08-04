076fe010094f529e1001cecc17541d0750da0e6a
dgSohn <nhos126@gmail.com>
2017-08-04 15:06:51 +0900

    add files

--- CocoaBaby/ViewController/BaseViewController.swift
+++ CocoaBaby/ViewController/BaseViewController.swift
@@ -20,9 +20,4 @@ class BaseViewController: UIViewController {
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Gradation2")!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
