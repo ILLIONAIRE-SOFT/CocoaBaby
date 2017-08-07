893c98c8b764cb99e9a060dac9be1cc54c86eb7a
dgSohn <nhos126@gmail.com>
2017-08-07 15:58:06 +0900

    update CocoaDateFormatter

--- CocoaBaby/Utilities/CocoaDateFormatter.swift
+++ CocoaBaby/Utilities/CocoaDateFormatter.swift
@@ -10,4 +10,16 @@ import Foundation
 
 class CocoaDateFormatter {
     
+    static func createComponents(from date: Date) -> DateComponents {
+        
+        let components = Calendar.current.dateComponents([.year, .month, .day], from: date)
+        return components
+    }
+    
+    static func createComponents(year: Int, month: Int, day: Int) -> DateComponents {
+        
+        let components = DateComponents(calendar: Calendar.current, timeZone: nil, era: nil, year: year, month: month, day: day, hour: nil, minute: nil, second: nil, nanosecond: nil, weekday: nil, weekdayOrdinal: nil, quarter: nil, weekOfMonth: nil, weekOfYear: nil, yearForWeekOfYear: nil)
+        
+        return components
+    }
 }
