bfa01d8d14f043d0cd1285d1c04fb73e2074a164
LeFal <qwertyhj2@gmail.com>
2017-08-07 15:09:35 +0900

    update tip tab

---
+++ CocoaBaby/TipsDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TipsDetailViewController.swift
+//  CocoaBaby
+//
+//  Created by LEOFALCON on 2017. 8. 7..
+//  Copyright © 2017년 Sohn. All rights reserved.
+//
+
+import UIKit
+
+class TipsDetailViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tipsDetailTableView: UITableView!
+    var tipList = ["지금 아기는요","엄마의 몸 상태","아빠는 이렇게 해주세요"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tipsDetailTableView.backgroundColor = UIColor(patternImage: UIImage(named: "Gradation2")!)
+        self.tipsDetailTableView.separatorStyle = .none
+
+        self.tipsDetailTableView.delegate = self
+        self.tipsDetailTableView.dataSource = self
+
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tipList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tipsDetailTableView.dequeueReusableCell(withIdentifier: "TipsDetailViewCell", for: indexPath) as! TipsTableViewCell
+        
+        cell.title.text = tipList[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+
+
+}
--- CocoaBaby/ViewController/TipsViewController.swift
+++ CocoaBaby/ViewController/TipsViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class TipsViewController: BaseViewController {
+class TipsViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var weeks = ["week 1", "week 2","week 3", "week 4", "week 5"]
+    
+    @IBOutlet var tipsTabelView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tipsTabelView.backgroundColor = UIColor(patternImage: UIImage(named: "Gradation2")!)
+        self.tipsTabelView.delegate = self
+        self.tipsTabelView.dataSource = self
+        
+        self.tipsTabelView.separatorStyle = .none
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
 
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weeks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tipsTabelView.dequeueReusableCell(withIdentifier: "TipsTabelViewCell", for: indexPath) as! TipsTableViewCell
+        
+        cell.title.text = self.weeks[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let tipsDetailViewController = storyBoard.instantiateViewController(withIdentifier: "TipsDetailViewController") as! TipsDetailViewController
+        
+        self.navigationController?.pushViewController(tipsDetailViewController, animated: true)
     }
 
 }
