b8e5d523079fb18115cd56fcb92a951346d096de
dadong <iluvdadong@naver.com>
2017-08-07 19:00:29 +0900

    update

--- CocoaBaby/ViewController/DiaryViewController.swift
+++ CocoaBaby/ViewController/DiaryViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class DiaryViewController: BaseViewController {
+class DiaryViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var diaryTableView: UITableView!
-    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         diaryTableView.backgroundColor = UIColor.mainBlueColor
+       // diaryTableView.estimatedRowHeight = 300
+        // diaryTableView.rowHeight = UITableViewAutomaticDimension
+        diaryTableView.delegate = self
+        diaryTableView.dataSource = self
         
     }
     
@@ -33,5 +36,22 @@ class DiaryViewController: BaseViewController {
             print("Load")
         }
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell") as! CustomTableViewCell
+        
+        
+        //다이어리를 생성한 날짜를 기준으로 출산일까지 D-day계산해서 표시
+       cell.cellLabel.text = "D-\(BabyStore.shared.getDday())" //
+        //cell.labelBackgroundView.tag = indexPath.row
+       // cell.labelBackgroundView.isUserInteractionEnabled = true
+        
+        return cell
+    }
+    
 
 }
