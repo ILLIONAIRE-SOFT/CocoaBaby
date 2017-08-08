6209c89a946b6c56f75dba7d4984fec730da4d04
dgSohn <nhos126@gmail.com>
2017-08-08 13:47:16 +0900

    add EditBabyViewController

---
+++ CocoaBaby/ViewController/EditBabyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditBabyViewController.swift
+//  CocoaBaby
+//
+//  Created by Sohn on 08/08/2017.
+//  Copyright © 2017 Sohn. All rights reserved.
+//
+
+import UIKit
+
+class EditBabyViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
