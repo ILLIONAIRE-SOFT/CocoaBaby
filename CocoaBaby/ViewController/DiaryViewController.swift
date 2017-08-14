d1778b5e5eb8d554fb88dfdd27984285cf0c18e3
dgSohn <nhos126@gmail.com>
2017-08-14 22:09:47 +0900

    fix home view

--- CocoaBaby/ViewController/DiaryViewController.swift
+++ CocoaBaby/ViewController/DiaryViewController.swift
@@ -96,12 +96,10 @@ class DiaryViewController: BaseViewController {
     // MARK: Methods
     func fetchDiaries() {
         startLoading()
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
         
         DiaryStore.shared.fetchDiaries(date: targetDate) {
             self.diaryTableView.reloadData()
             self.stopLoading()
-            UIApplication.shared.isNetworkActivityIndicatorVisible = false
             
             if self.refreshControl.isRefreshing {
                 self.refreshControl.endRefreshing()
