c3a43aed66923ff7f5dfae03c31aaf7bba593346
LeFal <qwertyhj2@gmail.com>
2017-08-14 11:20:30 +0900

    add tips detail tableview cell files

---
+++ CocoaBaby/TIpsDetailContentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TIpsDetailContentTableViewCell.swift
+//  CocoaBaby
+//
+//  Created by LEOFALCON on 2017. 8. 14..
+//  Copyright © 2017년 Sohn. All rights reserved.
+//
+
+import UIKit
+
+class TIpsDetailContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
