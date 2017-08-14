25f86d5f099f9b432ef7c17be68920500e270a67
dgSohn <nhos126@gmail.com>
2017-08-14 15:49:32 +0900

    update BabyStore

--- CocoaBaby/Model/BabyStore.swift
+++ CocoaBaby/Model/BabyStore.swift
@@ -56,10 +56,8 @@ class BabyStore {
             case let .success(baby):
                 self.baby = baby
                 completion(baby)
-            case let .failure(_):
+            case .failure(_):
                 completion(nil)
-            default:
-                return
             }
         }
     }
@@ -74,161 +72,73 @@ class BabyStore {
                 completion(result)
             }
         }
-//        let context = persistentContainer.viewContext
-//        
-//        loadBaby()
-//        
-//        context.performAndWait {
-//            if self.baby == nil {
-//                self.baby = Baby(context: context)
-//                self.baby.createdAt = Date() as NSDate
-//            }
-//            
-//            guard
-//                let name = name,
-//                let birthDate = birthDate,
-//                let pregnantDate = pregnantDate else {
-//                    print(BabyError.invalidInput)
-//                    return
-//            }
-//            
-//            self.baby.expectedBirthDate = birthDate as NSDate
-//            self.baby.expectedPregnantDate = pregnantDate as NSDate
-//            self.baby.name = name
-//            
-//            do {
-//                try context.save()
-//            } catch let error {
-//                print(error)
-//            }
-//        }
     }
     
     func updateBaby(name: String?, pregnantDate: Date?, birthDate: Date?, completion: ((Baby) -> ())?) {
         
-//        let context = persistentContainer.viewContext
-//        
-//        context.performAndWait {
-//            if self.baby == nil {
-//                self.baby = Baby(context: context)
-//                self.baby.createdAt = Date() as NSDate
-//            }
-//            
-//            if let name = name {
-//                self.baby.name = name
-//            }
-//            
-//            if let pregnantDate = pregnantDate {
-//                self.baby.expectedPregnantDate = pregnantDate as NSDate
-//            }
-//            
-//            if let birthDate = birthDate {
-//                self.baby.expectedBirthDate = birthDate as NSDate
-//            }
-//            
-//            do {
-//                try context.save()
-//            } catch let error {
-//                print(error)
-//            }
-//            
-//            if let completion = completion {
-//                OperationQueue.main.addOperation {
-//                    completion(self.baby)
-//                }
-//            }
-//        }
     }
     
     func deleteBaby() {
         
-//        let context = persistentContainer.viewContext
-//        
-//        if let baby = self.baby {
-//            context.delete(baby)
-//        } else {
-//            return
-//        }
-//        
-//        context.performAndWait {
-//            do {
-//                try context.save()
-//            } catch let error {
-//                print(error)
-//            }
-//        }
     }
     
     func getDday() -> DdayResult {
         
         var result = DdayResult(value: 0, mark: "+")
         
-//        guard let baby = self.baby else {
-//            return result
-//        }
-//        
-//        let calendar = Calendar.current
-//        let startDate = calendar.startOfDay(for: Date())
-//        
-//        guard let expectedBirthDate = baby.expectedBirthDate else {
-//            return result
-//        }
-//        
-//        let endDate = calendar.startOfDay(for: expectedBirthDate as Date)
-//        let components = calendar.dateComponents([.day], from: startDate, to: endDate)
-//        
-//        guard let value = components.day else {
-//            return result
-//        }
-//        
-//        if value >= 0 {
-//            result.value = value
-//            result.mark = "-"
-//        } else {
-//            result.mark = "+"
-//            result.value = abs(value)
-//        }
+        guard let baby = self.baby else {
+            return result
+        }
+
+        let calendar = Calendar.current
+        let startDate = calendar.startOfDay(for: Date())
+
+        let endDate = calendar.startOfDay(for: Date(timeIntervalSince1970: baby.birthDate))
+        let components = calendar.dateComponents([.day], from: startDate, to: endDate)
+
+        guard let value = components.day else {
+            return result
+        }
+        
+        if value >= 0 {
+            result.value = value
+            result.mark = "-"
+        } else {
+            result.mark = "+"
+            result.value = abs(value)
+        }
         
         return result
     }
     
     func getName() -> String {
         
-//        guard let baby = self.baby else {
-//            return "No Baby"
-//        }
-//        
-//        if let name = baby.name {
-//            return name
-//        } else {
-//            return "No Name"
-//        }
-        return ""
+        guard let baby = self.baby else {
+            return "No Baby"
+        }
+        
+        return baby.name
     }
     
     func getPregnantWeek() -> Week {
         
         var week = Week(week: 0, dayOfWeek: 0)
         
-//        guard let baby = self.baby else {
-//            print(BabyError.invalidBaby)
-//            return week
-//        }
-//        
-//        let calendar = Calendar.current
-//        let today = calendar.startOfDay(for: Date())
-//        
-//        guard let expectedPregnantDate = baby.expectedPregnantDate else {
-//            return week
-//        }
-//        
-//        let pregnantDate = calendar.startOfDay(for: expectedPregnantDate as Date)
-//        let components = calendar.dateComponents([.day], from: pregnantDate, to: today)
-//        
-//        if let day = components.day {
-//            week.week = (day - 1) / 7 + 1
-//            week.dayOfWeek = (day - 1) % 7 + 1
-//        }
+        guard let baby = self.baby else {
+            print(BabyError.invalidBaby)
+            return week
+        }
+
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+
+        let pregnantDate = calendar.startOfDay(for: Date(timeIntervalSince1970: baby.pregnantDate))
+        let components = calendar.dateComponents([.day], from: pregnantDate, to: today)
+
+        if let day = components.day {
+            week.week = (day - 1) / 7 + 1
+            week.dayOfWeek = (day - 1) % 7 + 1
+        }
         
         return week
     }
--- CocoaBaby/ViewController/SplashViewController.swift
+++ CocoaBaby/ViewController/SplashViewController.swift
@@ -60,20 +60,4 @@ class SplashViewController: UIViewController {
         appDelegate.window?.makeKeyAndVisible()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
