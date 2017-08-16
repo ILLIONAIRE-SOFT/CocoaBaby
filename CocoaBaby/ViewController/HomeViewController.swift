e9e017d45fc81bcb4bec0b5e29ccbacedab5a7b8
dadong <iluvdadong@naver.com>
2017-08-16 14:16:48 +0900

    update

--- CocoaBaby/ViewController/HomeViewController.swift
+++ CocoaBaby/ViewController/HomeViewController.swift
@@ -20,8 +20,6 @@ class HomeViewController: BaseViewController {
     @IBOutlet weak var waterDrop: UIView!
     @IBOutlet weak var waterDropTwo: UIView!
 
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -49,16 +47,62 @@ class HomeViewController: BaseViewController {
       
         }, completion: nil)
       
-        // MARK: WaterDropTwo
+        // WaterDropTwo
         waterDropTwo.layer.cornerRadius = 3
         // position
         UIView.animate(withDuration: 5, delay: 1, options: [.repeat], animations: {
-            self.waterDropTwo.frame.origin.y = 800
+            self.waterDropTwo.frame.origin.y = 80
             self.waterDropTwo.alpha = 1.0
             self.waterDropTwo.alpha = 0.0
             
         }, completion: nil)
+     
         
+        // WaterDropThree
+        var waterDropThree = UIView(frame: CGRect(x: 170, y: 480, width: 8, height: 8))
+        waterDropThree.backgroundColor = UIColor.init(colorWithHexValue: 0xE3BCC3, alpha: 1)
+        waterDropThree.layer.cornerRadius = 4
+        
+        self.view.addSubview(waterDropThree)
+        // position
+        UIView.animate(withDuration: 3, delay: 1.5, options: [.repeat], animations: {
+            
+            waterDropThree.frame.origin.y = 400
+            waterDropThree.alpha = 1.0
+            waterDropThree.alpha = 0.0
+            
+        }, completion: nil)
+
+        
+        // WaterDropfour
+        var waterDropfour = UIView(frame: CGRect(x: 200, y: 480, width: 5, height: 5))
+        waterDropfour.backgroundColor = UIColor.init(colorWithHexValue: 0xE3BCC3, alpha: 1)
+        waterDropfour.layer.cornerRadius = 2
+        
+        self.view.addSubview(waterDropfour)
+        // position
+        UIView.animate(withDuration: 5, delay: 0.5, options: [.repeat], animations: {
+            
+            waterDropfour.frame.origin.y = 400
+            waterDropfour.alpha = 1.0
+            waterDropfour.alpha = 0.0
+            
+        }, completion: nil)
+        
+        // WaterDropfive
+        var waterDropfive = UIView(frame: CGRect(x: 240, y: 470, width: 6, height: 6))
+        waterDropfive.backgroundColor = UIColor.init(colorWithHexValue: 0xE3BCC3, alpha: 1)
+        waterDropfive.layer.cornerRadius = 3
+        
+        self.view.addSubview(waterDropfive)
+        // position
+        UIView.animate(withDuration: 4, delay: 0.5, options: [.repeat], animations: {
+            
+            waterDropfive.frame.origin.y = 340
+            waterDropfive.alpha = 1.0
+            waterDropfive.alpha = 0.0
+            
+        }, completion: nil)
     }
     
     
