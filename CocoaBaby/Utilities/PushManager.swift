1892d3716668923cfab370b98fb2eeee3991a635
DaiGeun Sohn <nhos126@gmail.com>
2017-08-17 19:21:12 +0900

    PushManager

--- CocoaBaby/Utilities/PushManager.swift
+++ CocoaBaby/Utilities/PushManager.swift
@@ -12,16 +12,19 @@ struct PushManager {
     
     static var serverURL: String = "http://13.124.140.81"
     static var port: String = "3000"
+    static var diaryNotiURL: String = "/notification/diary"
     
     static func pushToPartner(deviceToken: String) {
         
-        guard let url = URL(string: "\(serverURL):\(port)/") else {
+        guard let url = URL(string: "\(serverURL):\(port)") else {
             return
         }
-        
         print(url)
+        
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
+
+//        var params = ["deviceToken": "1234"]
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
