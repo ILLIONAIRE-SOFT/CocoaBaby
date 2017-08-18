6aff7018089f86b710487485241f7d3529f943c1
DaiGeun Sohn <nhos126@gmail.com>
2017-08-18 15:03:16 +0900

    add comment image view to diaryViewController

--- CocoaBaby/CustomTableViewCell.swift
+++ CocoaBaby/CustomTableViewCell.swift
@@ -9,19 +9,13 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-   
+    
     @IBOutlet var labelBackgroundView: UIView!
     @IBOutlet var contentsLabel: UILabel!
     @IBOutlet var addtionalDate: UILabel!
     @IBOutlet var weekLabel: UILabel!
+    @IBOutlet var flagCommentImageView: UIImageView!
     
-    override func layoutSubviews() {
-        if weekLabel.text == "SAT" || weekLabel.text == "SUN" {
-            self.weekLabel.textColor = UIColor.init(colorWithHexValue: 0xDF6C7E)
-        } else {
-            self.weekLabel.textColor = UIColor.darkGray
-        }
-    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -30,10 +24,10 @@ class CustomTableViewCell: UITableViewCell {
         self.selectionStyle = UITableViewCellSelectionStyle.none
         
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
@@ -42,9 +36,16 @@ class CustomTableViewCell: UITableViewCell {
         self.addtionalDate.text = "\(diary.date.day)"
         self.weekLabel.text = "\(CocoaDateFormatter.getWeekDay(from: diary))"
         
-           }
-    
-    //만약에 contentsLabel의 텍스트가 공백이면 동그라미로
-
-
+        if let comment = diary.comment {
+            if comment != "" {
+                flagCommentImageView.isHighlighted = true
+            }
+        }
+        
+        if weekLabel.text == "SAT" || weekLabel.text == "SUN" {
+            self.weekLabel.textColor = UIColor.init(colorWithHexValue: 0xDF6C7E)
+        } else {
+            self.weekLabel.textColor = UIColor.darkGray
+        }
+    }
 }
