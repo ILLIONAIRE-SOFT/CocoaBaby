0ddce133aeba240a30c95fd6a77e4b849be78fea
DaiGeun Sohn <nhos126@gmail.com>
2017-08-18 13:01:17 +0900

    make FireBaseBabyAPI

--- CocoaBaby/Model/FireBaseAPI.swift
+++ CocoaBaby/Model/FireBaseAPI.swift
@@ -35,12 +35,6 @@ enum DiaryPayloadName: String {
     case comment = "comment"
 }
 
-enum BabyPayloadName: String {
-    case name = "name"
-    case pregnantDate = "pregnantDate"
-    case birthDate = "birthDate"
-}
-
 enum TipsPayloadName: String {
     case babyTitle = "babyTitle"
     case babyContent = "babyContent"
@@ -58,11 +52,6 @@ enum SharePayloadName: String {
 
 enum DiaryResult {
     case success(Diary)
-    case failure(Error)
-}
-
-enum BabyResult {
-    case success(Baby?)
     case failure(Error)
 }
 
@@ -213,102 +202,6 @@ struct FireBaseAPI {
     }
     
 }
-
-// MARK: - Baby
-extension FireBaseAPI {
-    
-    static func saveBaby(baby: Baby, completion: @escaping (BabyResult)->()) {
-        guard let uid = Auth.auth().currentUser?.uid else {
-            print(FireBaseAPIError.invalidUser)
-            return
-        }
-        
-        let post = [
-            BabyPayloadName.name.rawValue: baby.name,
-            BabyPayloadName.pregnantDate.rawValue: baby.pregnantDate,
-            BabyPayloadName.birthDate.rawValue: baby.birthDate,
-            ] as [String : Any]
-        
-        ref.child(FireBaseDirectoryName.babies.rawValue).child("\(uid)").setValue(post, andPriority: nil) { (error, ref) in
-            if let error = error {
-                print(error)
-                completion(BabyResult.failure(error))
-            } else {
-                completion(BabyResult.success(baby))
-            }
-        }
-    }
-    
-    static func fetchBaby(completion: @escaping (BabyResult) -> ()) {
-        guard let uid = Auth.auth().currentUser?.uid else {
-            return
-        }
-        
-        ref.child(FireBaseDirectoryName.babies.rawValue).child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
-            
-            guard snapshot.exists() else {
-                completion(BabyResult.failure(FireBaseAPIError.noBaby))
-                return
-            }
-            
-            var result: Baby? = nil
-            
-            let dict = snapshot.value as! [String:Any]
-            if let baby = baby(from: dict) {
-                result = baby
-                
-                completion(BabyResult.success(result))
-                
-            } else {
-                completion(BabyResult.failure(FireBaseAPIError.noBaby))
-            }
-            
-        })
-    }
-    
-    static func updateBaby(baby: Baby, completion: @escaping (BabyResult)->()) {
-        guard let uid = Auth.auth().currentUser?.uid else {
-            print(FireBaseAPIError.invalidUser)
-            return
-        }
-        
-        let post = [
-            BabyPayloadName.name.rawValue: baby.name,
-            BabyPayloadName.pregnantDate.rawValue: baby.pregnantDate,
-            BabyPayloadName.birthDate.rawValue: baby.birthDate,
-            ] as [String : Any]
-
-        ref.child(FireBaseDirectoryName.babies.rawValue).child(uid).updateChildValues(post) { (error, ref) in
-            if let error = error {
-                print(error)
-                completion(BabyResult.failure(error))
-            } else {
-                completion(BabyResult.success(baby))
-            }
-        }
-    }
-
-    
-    
-    static private func baby(from json: [String: Any]) -> Baby? {
-        var baby = Baby()
-        
-        guard
-            let name = json[BabyPayloadName.name.rawValue],
-            let pregnantDate = json[BabyPayloadName.pregnantDate.rawValue],
-            let birthDate = json[BabyPayloadName.birthDate.rawValue] else {
-                return nil
-        }
-        
-        baby.name = name as! String
-        baby.pregnantDate = pregnantDate as! Double
-        baby.birthDate = birthDate as! Double
-        
-        return baby
-    }
-}
-
-
 
 // MARK: - Tips
 
