7fd1c62f96ee5ab1226131f94f0c247c5afe3924
LeFal <qwertyhj2@gmail.com>
2017-08-23 14:33:48 +0900

    camera linked with shareView

--- CocoaBaby/CameraViewController_camera.swift
+++ CocoaBaby/CameraViewController_camera.swift
@@ -107,11 +107,24 @@ extension CameraViewController : AVCapturePhotoCaptureDelegate {
             let image = UIImage(data: jpegData)
             let cropImage = ImageService.cropImageToSquare(image: image!)
             
-            _ = ImageService.merge(image: cropImage!, cameraViewLabel: self.weekUILabel, cameraViewWidth: self.cameraView.frame.width, babyNameViewLabel: self.babyNameUILabel)
+            let mergeImage = ImageService.merge(image: cropImage!, cameraViewLabel: self.weekUILabel, cameraViewWidth: self.cameraView.frame.width, babyNameViewLabel: self.babyNameUILabel)
+            showShareViewController(image: mergeImage)
+            
             self.cameraView.backgroundColor = UIColor(patternImage: image!)
+            
+            
         }
     }
     
+    func showShareViewController(image : UIImage?) {
+        let shareSB = UIStoryboard(name: StoryboardName.share, bundle: nil)
+        let captureBabyViewController = shareSB.instantiateViewController(withIdentifier: StoryboardName.captureBabyViewController) as! CaptureBabyViewController
+        if let image = image {
+            captureBabyViewController.selectedImage = image
+        }
+        captureBabyViewController.modalPresentationStyle = .overCurrentContext
+        self.present(captureBabyViewController, animated: true, completion: nil)
+    }
     
     @IBAction func changeCamera(_ sender: Any) {
         frontCamera = !frontCamera
