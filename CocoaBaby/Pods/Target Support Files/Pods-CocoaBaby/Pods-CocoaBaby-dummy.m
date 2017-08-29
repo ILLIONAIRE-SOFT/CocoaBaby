c465ce413422a6ec9022da122ff127c0c9cd610b
DaiGeun Sohn <nhos126@gmail.com>
2017-08-29 17:08:20 +0900

    move files

--- CocoaBaby/Pods/Target Support Files/Pods-CocoaBaby/Pods-CocoaBaby-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CocoaBaby : NSObject
-@end
-@implementation PodsDummy_Pods_CocoaBaby
-@end
--- CocoaBaby/Pods/Target Support Files/Pods-CocoaBabyTests/Pods-CocoaBabyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CocoaBabyTests : NSObject
-@end
-@implementation PodsDummy_Pods_CocoaBabyTests
-@end
